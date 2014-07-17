6ee1e0a872f921d0c316a08b24c0e3dac6215246
= <yhk04@mail.aub.edu>
2014-07-17 00:15:04 -0400

    add user models

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'bootstrap-sass', '>= 2.3.2.1', '< 3.0'
 gem 'sprockets', '2.11.0'
 gem 'bcrypt-ruby', '3.1.2'
 gem 'devise'
+gem 'simple_form'
+
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   get 'about' => 'pages#about'
   root 'pages#home'
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
